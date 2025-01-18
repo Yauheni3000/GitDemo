42b2e47fc3c54a0b3113e6c688905af55573ff21
yauheni <yursha9971@gmail.com>
2025-01-18 20:05:48 +0100

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Yauheni Yursha on 18/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
